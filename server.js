b4a649098e31b7aaf8f925b74f98ef0d8403a2a1
moxiecui <moxiecui@gmail.com>
2016-04-23 23:04:14 -0700

    add PUT todos/:id

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos/
 app.post('/todos', function (req, res) {
-	// var body = _.pick(req.body, "description", "completed");
-	var body = req.body;
+	var body = _.pick(req.body, "description", "completed");
+	// var body = req.body;
 	if (!(_.isBoolean(body.completed) && _.isString(body.description)) || body.description.trim().length  === 0) {
 		return res.status(400).send();
 	}
@@ -47,8 +47,8 @@ app.post('/todos', function (req, res) {
 	body.description = body.description.trim();
 	todoNextId += 1;
 	// console.log(_.pick(body, "id", "description", "completed"))
-	todos.push(_.pick(body, "id", "description", "completed"));
-	// todos.push(body);
+	// todos.push(_.pick(body, "id", "description", "completed"));
+	todos.push(body);
 
 	res.json(body);
 });
@@ -66,6 +66,33 @@ app.delete('/todos/:id', function (req, res) {
 	}
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var body = _.pick(req.body, "description", "completed");
+	var validAttributes = {};
+
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (!matchedTodo) {
+		return res.status(404).json({"error": "no todo item found with id " + todoId});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && 
+			body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');

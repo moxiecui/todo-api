c85b17a2c9e588a3ffeab3a1bf9abb1981e5a545
moxiecui <moxiecui@gmail.com>
2016-04-24 21:57:25 -0700

    convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -99,33 +99,33 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, "description", "completed");
-	var validAttributes = {};
+	var attributes = {};
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
-	if (!matchedTodo) {
-		return res.status(404).json({
-			"error": "no todo item found with id " + todoId
+
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	}
+
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	}
+
+	db.todo.findById(todoId)
+		.then(function(todo) {
+			if (todo) {
+				todo.update(attributes).then(function(todo) {
+					res.json(todo.toJSON());
+				}, function(e) {
+					res.status(400).json(e);
+				});
+
+			} else {
+				res.status(404).send();
+			}
+		}, function(e) {
+			res.status(500).json(e);
 		});
-	}
-
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
-
-	if (body.hasOwnProperty('description') && _.isString(body.description) &&
-		body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function() {

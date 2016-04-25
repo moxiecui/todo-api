7cc4c66bee3734ac3905ac12487cdd08346c71c1
moxiecui <moxiecui@gmail.com>
2016-04-24 23:19:31 -0700

    add user model and post users request

--- server.js
+++ server.js
@@ -128,6 +128,22 @@ app.put('/todos/:id', function(req, res) {
 		});
 });
 
+
+// ==================== USER MODEL ===================
+// POST /users
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, "email", "password");
+
+	db.user.create(body)
+		.then(function(user) {
+			res.json(user.toJSON());
+		}, function(error) {
+			res.status(400).json(error);
+		});
+});
+
+
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');

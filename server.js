0e130c86d0f5fd21477c5e5ef3c7faeef43971ea
moxiecui <moxiecui@gmail.com>
2016-04-24 23:43:58 -0700

    hash and salt password

--- server.js
+++ server.js
@@ -142,6 +142,18 @@ app.post('/users', function(req, res) {
 		});
 });
 
+// POST /users/login
+app.port('/users/login', function(req, res) {
+	var body = _.pick(req.body, "email", "password");
+
+	if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return res.status(400).send();
+	}
+
+	res.json(body);
+
+});
+
 
 
 db.sequelize.sync(

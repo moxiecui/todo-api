63a40fc62cc380c7c6468d9e39ef3c2140dfe91a
moxiecui <moxiecui@gmail.com>
2016-04-24 21:05:42 -0700

    convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -19,50 +19,54 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=house
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where })
+	.then(function(todos) {
+		res.json(todos);
+	}, function(error) {
+		res.status(500).send();
+	});
+
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.findById(todoId)
+	.then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (error) {
+		res.status(500).send();
 	});
-
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
 });
 
 // POST /todos/
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, "description", "completed");
 	// var body = req.body;
-	if (!(_.isBoolean(body.completed) && _.isString(body.description)) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	// if (!(_.isBoolean(body.completed) && _.isString(body.description)) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
 	// console.log('description: ' + body.description);
 	// body.id = todoNextId;

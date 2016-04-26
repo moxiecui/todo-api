79558bd57e6a653f7afd53d8f9a5df19e933b1d0
moxiecui <moxiecui@gmail.com>
2016-04-25 21:26:51 -0700

    Add DELETE /users/login

--- server.js
+++ server.js
@@ -162,22 +162,30 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, "email", "password");
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
-		token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		} else {
-			res.status(401).send();
-
-		}
-	}, function(error) {
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(e) {
+		// console.log(e);
 		res.status(401).send();
 	});
 });
 
-
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
+	});
+});
 
 db.sequelize.sync({
 	force: true
